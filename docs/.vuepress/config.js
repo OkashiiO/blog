1439789c504bbf7cc593fe68d8ef6bb64ab0ec90
ljco_o <1900849346@qq.com>
2020-06-11 19:58:45 +0800

    第二次提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,24 +1,23 @@
 module.exports = {
-    base: '/blog/',
-    title: 'Hello VuePress',
-    description: 'Just playing around'
-}
+    base: '/blog/',         //站点根路径
+    title: '李嘉诚的博客',  //网站标题
+    dest: './dist',
 
-module.exports = {
+
     themeConfig: {
-      repo: 'https://github.com/136917909/blog',
-      //自定义仓库链接文字
-      reppLabel: 'My GitHub',
-
-      //导航栏
-      nav: [
-          { text: 'Home',link:'/' },
-          { text: 'FirstBlog', link:'/blog/HTML.md'}
-      ],
-      sidebar: [
-          ['/','首页'],
-          ['blog/HTML.md','HTML'],
-          ['blog/CSS.md','CSS']
-      ]
-    }
+        repo: 'https://github.com/136917909/blog',
+        //自定义仓库链接文字
+        reppLabel: 'My GitHub',
+  
+        //导航栏
+        nav: [
+            { text: 'Home',link:'/' },
+            { text: 'FirstBlog', link:'/blog/HTML.md'}
+        ],
+        sidebar: [
+            ['/','首页'],
+            ['blog/HTML.md','HTML'],
+            ['blog/CSS.md','CSS']
+        ]
+      }
 }
